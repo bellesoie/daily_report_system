dc98cbc5b618b97b74a88da2fb69576abb67ef59
azumakinuka <azumakinuka@localhost>
2024-02-07 17:49:14 +0900

    DBUtil作成

---
+++ src/main/java/constants/DBUtil.java
@@ -0,0 +1,25 @@
+package constants;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class DBUtil {
+
+    private static EntityManagerFactory emf;
+
+    //EntityManagerインスタンスを生成
+    public static EntityManager createEntityManager() {
+        return _getEntityManagerFactory().createEntityManager();
+    }
+
+    //EntityManagetFactoryインスタンスを生成
+    private static EntityManagerFactory _getEntityManagerFactory() {
+        if (emf == null) {
+            emf = Persistence.createEntityManagerFactory(JpaConst.PERSISTENCE_UNIT_NAME);
+        }
+
+        return emf;
+    }
+
+}
